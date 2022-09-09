8b784eed910e8e779471832df6069517098d4e59
araromi2 <abdulhameedararomi@gmail.com>
2022-09-09 09:40:58 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,11 @@ int main(void)
 	c = 'a';
 	while (c <= 'z')
 	{
-		if (c != 'e' && c != 'q')
+		if (c != 'q' && c != 'e')
 		{
 			putchar(c);
-			c++;
 		}
+		c++;
 	}
 	putchar('\n');
 	return (0);
