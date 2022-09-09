8273bf1580bed72c9624ef05aeed51904ab25a04
araromi2 <abdulhameedararomi@gmail.com>
2022-09-09 12:04:31 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int n1;
 	int n2;
 
-	for (n1='0'; n1<='8'; n1++)
+	for (n1 = '0'; n1 <= '8'; n1++)
 	{
-		for (n2='0'; n2<='9'; n2++)
+		for (n2 = '0'; n2 <= '9'; n2++)
 		{
 			if (n1 < n2 && n1 != n2)
 			{
