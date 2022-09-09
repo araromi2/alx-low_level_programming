67219b6f003939a4e88af1b2f2744ff8f0338d91
araromi2 <abdulhameedararomi@gmail.com>
2022-09-09 09:49:52 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
