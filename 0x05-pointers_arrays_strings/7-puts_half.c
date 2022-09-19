0005376c88ee4714d02de40ba6299a90ede271d2
araromi2 <abdulhameedararomi@gmail.com>
2022-09-19 14:30:58 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -31,7 +31,7 @@ void puts_half(char *str)
 	if ((len % 2) == 0)
 		half_len = len / 2;
 	else
-		half_len = ((len - 1) / 2) - 1;
+		half_len = ((len - 1) / 2) + 1;
 
 	for (i = half_len; str[i] != '\0'; i++)
 	{
