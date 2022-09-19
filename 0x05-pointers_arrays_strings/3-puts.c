24cdabfc1089c3cdd7feef26cc0efe60b7717e85
araromi2 <abdulhameedararomi@gmail.com>
2022-09-19 11:54:29 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: String to be printed
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
