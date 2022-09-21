43de7b813c24f475711b955c918e0e5c07810991
araromi2 <abdulhameedararomi@gmail.com>
2022-09-21 10:55:11 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * str_len - Find the length of a string
- * @string: the string
- *
- * Return: the length of the string.
- */
-int str_len(char *string)
-{
-        int i = 0;
-
-        while (string[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
-}
-
-/**
  * _strcat - concatenates two strings
  * @dest: destination string
  * @src: source string
@@ -26,14 +9,18 @@ int str_len(char *string)
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, len;
+	int i, j;
 
-	len = str_len(dest);
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[len + i] = src[i];
+		dest[j + i] = src[i];
 	}
-	dest[len + i] = '\0';
+	dest[j + i] = '\0';
 	return (dest);
 }
 
