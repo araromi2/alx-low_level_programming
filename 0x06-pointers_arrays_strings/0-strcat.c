ce3b6501eb05d01b8ceacb055df49039c3bd3083
araromi2 <abdulhameedararomi@gmail.com>
2022-09-21 10:38:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
+ * str_len - Find the length of a string
+ * @string: the string
  *
  * Return: the length of the string.
  */
-int _strlen(char *s)
+int str_len(char *string)
 {
         int i = 0;
 
-        while (s[i] != '\0')
+        while (string[i] != '\0')
         {
                 i++;
         }
@@ -28,7 +28,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i, len;
 
-	len = _strlen(dest);
+	len = str_len(dest);
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
