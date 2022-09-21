37e11cd1255c8c5177fa6ca4dfacd8ea5911b799
araromi2 <abdulhameedararomi@gmail.com>
2022-09-21 06:39:49 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+        return (i);
+}
+
+/**
+ * _strncat - concatenates two strings at most n bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to concatenate
+ *
+ * Return: concatenated string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = _strlen(dest);
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
+
