a779ba831af3b8283f088f4f049c5ace7710982a
araromi2 <abdulhameedararomi@gmail.com>
2022-09-21 12:16:02 -0700

    6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ *
+ * Return: capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int j, k;
+	int character[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
+		')', '{', '}'};
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (k = 0; k < 13; k++)
+		{
+			if (s[j] == character[k] && s[j + 1] != '\0')
+			{
+				if((s[j + 1] >= 'a') && (s[j + 1] <= 'z'))
+				{
+					s[j + 1] -= 32;
+				}
+				break;
+			}
+		}
+	}
+	return (s);
+}
