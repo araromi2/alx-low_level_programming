cba3394e13dcf76c2344ed08c857c2e4345ba5b6
araromi2 <abdulhameedararomi@gmail.com>
2022-09-22 08:28:16 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,7 +16,7 @@ void print_buffer(char *b, int size)
 		printf("\n");
 	else
 	{
-		for(i = 0; i < size; i += 10)
+		for (i = 0; i < size; i += 10)
 		{
 			printf("%08x: ", i);
 			for (j = 0; j < 10; j++)
