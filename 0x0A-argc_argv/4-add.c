ae504970f4f3d7d7642d5f6b8ba50b11d6329cc9
araromi2 <abdulhameedararomi@gmail.com>
2022-09-30 07:38:44 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - Entry function: A program that adds positive numbers
+ * @argc: number of arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 if success and 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, add = 0;
+
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(**(argv + i)))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
