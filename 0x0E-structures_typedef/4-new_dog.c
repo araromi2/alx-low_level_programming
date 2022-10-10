98c499dd29cb1baba853c5ff4807f5dcd80f7b52
araromi2 <abdulhameedararomi@gmail.com>
2022-10-10 15:33:32 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates an instance of a dog
@@ -13,13 +14,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int len1, len2;
+	char *name_copy, *owner_copy;
 
+	len1 = strlen(name);
+	len2 = strlen(owner);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	name_copy = malloc(sizeof(*name_copy) * len1 + 1);
+	if (name_copy == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	owner_copy = malloc(sizeof(*owner_copy) * len2 + 1);
+	if (owner_copy == NULL)
+	{
+		free(dog);
+		free(name_copy);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
+
+	dog->name = name_copy;
 	dog->age = age;
-	dog->owner = owner;
-	return(dog);
+	dog->owner = owner_copy;
+	return (dog);
 }
 
