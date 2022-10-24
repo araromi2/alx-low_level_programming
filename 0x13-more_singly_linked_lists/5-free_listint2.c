d7f9712b303401422b67d9be0d92540d13fd6eb9
araromi2 <abdulhameedararomi@gmail.com>
2022-10-24 15:54:05 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *current = *head;
 
-	while (*head != NULL)
+	while (current != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
 	*head = NULL;
 }
