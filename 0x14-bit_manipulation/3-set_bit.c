7ddf97554c17c8a45b0e67588484fb220ee72945
araromi2 <abdulhameedararomi@gmail.com>
2022-10-28 17:52:47 -0700

    3-setq

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!n || index >= (sizeof(unsigned long int) + 8))
+	if (!n || index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n ^= (1 << index);
+	*n |= (1 << index);
 	return (1);
 }
 
