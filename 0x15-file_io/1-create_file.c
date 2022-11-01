617ccc3e91e266992d4ef9f84fd597fd0080881d
araromi2 <abdulhameedararomi@gmail.com>
2022-10-31 20:15:50 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	for (i = 0; *(text_content + i) != '\0'; i++)
-		;
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
+
+		for (i = 0; *(text_content + i) != '\0'; i++)
+			;
 		temp = write(fd, text_content, i);
 		if (temp == -1 || i != temp)
 			return (-1);
