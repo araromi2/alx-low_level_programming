9a5b2088ae35a4063fe053bde0754e9b7b8ba091
araromi2 <abdulhameedararomi@gmail.com>
2022-10-31 19:58:08 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the content to placed in the file
+ *
+ * The creted file must have those permissions: rw-------
+ * If the file already exists, do not change the permissions
+ * If the file already exists, truncate it
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, temp;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		temp = write(fd, text_content, i);
+		if (temp == -1 || i != temp)
+			return (-1);
+	}
+	if (close(fd) < 0)
+		return (-1);
+	return (1);
+}
+
